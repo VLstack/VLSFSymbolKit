2fc0b753a623f997ca1b5b732fbedfbc16e53c51
VLstack <github@vlstack.fr>
2025-04-28 16:21:17 +0200

    debug

--- Sources/VLSFSymbolKit/SFSymbolPicker/SFSymbolPickerSheet.swift
+++ Sources/VLSFSymbolKit/SFSymbolPicker/SFSymbolPickerSheet.swift
@@ -175,6 +175,7 @@ extension VLstack
     .onChange(of: currentSymbolVariant) { taskId += 1 }
     .task(id: taskId)
     {
+     print("taskId: \(taskId)")
      isFiltering = true
      Task
      {
@@ -187,6 +188,7 @@ extension VLstack
 
       let filtered = await grouper.filter(search: search,
                                           symbolVariant: currentSymbolVariant?.stringEncoded ?? "")
+      print("task.filtered.count: \(filtered.count)")
       await MainActor.run
       {
        self.groups = filtered
@@ -201,9 +203,11 @@ extension VLstack
     ProgressView()
     .task
     {
+     print("task loadGroups")
      await grouper.load()
      let filtered = await grouper.filter(search: search,
                                          symbolVariant: currentSymbolVariant?.stringEncoded ?? "")
+     print("filtered.count \(filtered.count)")
      await MainActor.run
      {
       self.groups = filtered
