300f407a370fdf0341e7f363bb0dea886a080f76
VLstack <github@vlstack.fr>
2025-05-05 10:21:26 +0200

    add Sendable SFSymbolDTO

---
+++ Sources/VLSFSymbolKit/SFSymbolDTO.swift
@@ -0,0 +1,20 @@
+import VLstackNamespace
+import SwiftUI
+
+public struct SFSymbolDTO: Equatable, Codable, Hashable, Sendable
+{
+ public let sfSymbol: VLstack.SFSymbol
+ private let encodedVariant: String
+
+ public var variant: VLstack.SFSymbolVariants
+ {
+  VLstack.SFSymbolVariants(stringEncoded: encodedVariant)
+ }
+
+ public init(_ sfSymbol: VLstack.SFSymbol,
+             variant: VLstack.SFSymbolVariants? = nil)
+ {
+  self.sfSymbol = sfSymbol
+  self.encodedVariant = ( variant ?? VLstack.SFSymbolVariants(SymbolVariants.none) ).stringEncoded
+ }
+}
